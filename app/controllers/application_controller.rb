81121e3a414bf50aa1c76136ea897d9ac037e0df
iori matumoto <machulive.822@gmail.com>
2021-11-27 20:44:59 +0900

    Basic認証の記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'saku' && password == '1919'
+    end
+  end  
 end
+
