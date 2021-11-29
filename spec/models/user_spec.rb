a7690646783654ba8417f4faa9e0c067cf80feb8
iori matumoto <machulive.822@gmail.com>
2021-11-29 22:33:51 +0900

    nickname,emailのバリデーションテストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,11 +5,18 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
   
-  describe 'ユーザー新規登録'　do  
-    it 'nickname:必須' do
-
+  describe 'ユーザー新規登録' do  
+    it 'nicknameが空では登録できない' do
+      @user.nickname = ''
+      @user.valid?
       
+      expect(@user.errors.full_messages).to include "Nickname can't be blank"
+    end
+    it 'emailが空では登録できない' do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email can't be blank"
+  
     end
   end
-
 end
