eff19c12d56f455334bd6a4e7a1374f405131912
iori matumoto <machulive.822@gmail.com>
2021-11-30 12:41:10 +0900

    単体テストコード（追加）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
+  PASSWORD_REGEX = /[a-z\d]{6,}/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
   
-  validates :last_name, presence: true#, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :first_name, presence: true#, format: { with: /^[ぁ-んァ-ン一-龥]/ }
-  #validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  #validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
   
-  #validates :birthday, presence: true
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナ文字を使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+  
+  
+  validates :birthday, presence: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,10 +4,10 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    #last_name             { Gimei.last.kanji }
-    #first_name            { Gimei.first.kanji }
-    #last_name_kana        { Gimei.last.katakana }
-    #first_name_kana       { Gimei.first.katakana }
-    #birthday              { Faker::Date.backward }
+    last_name             { Gimei.last.kanji }
+    first_name            { Gimei.first.kanji }
+    last_name_kana        { Gimei.last.katakana }
+    first_name_kana       { Gimei.first.katakana }
+    birthday              { Faker::Date.backward }
   end
 end
