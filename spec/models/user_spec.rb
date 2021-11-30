c3dc02734455fec09f4cff1cc824351af2fde124
iori matumoto <machulive.822@gmail.com>
2021-11-30 10:55:21 +0900

    空では登録できない、テストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,14 +6,20 @@ RSpec.describe User, type: :model do
   end
   
   describe 'ユーザー新規登録' do  
-    #context '新規登録が上手く行く時' do
-      #it 'passwordが６文字以上の半角英数字混合であれば登録できる'  do  
-        #@user.password = "000aaa"
-        #@user.password_confirmation = "000aaa"
+    #context '新規登録ができる場合' do
+      #it "全ての項目に入力できている" do
         #expect(@user).to be_valid
-      #end  
-    #null: false、presence: true のテスト
+      #end
 
+     #it 'passwordが６文字以上の半角英数字混合であれば登録できる'  do  
+        #@user.password = "000aaa"
+       # @user.password_confirmation = "000aaa"
+        #expect(@user).to be_valid
+        
+      #end 
+    #end   
+    #null: false、presence: true のテスト
+    context '新規登録ができない場合'
       it 'nickname:必須' do
         @user.nickname = ''
         @user.valid?
@@ -29,6 +35,7 @@ RSpec.describe User, type: :model do
       it 'passwaord:必須' do
         @user.password = ''
         @user.valid?
+        
         expect(@user.errors.full_messages).to include "Password can't be blank"
       end
       
@@ -36,33 +43,40 @@ RSpec.describe User, type: :model do
         @user.password = '12345'
         @user.password_confirmation = '12345'
         @user.valid?
+        
         expect(@user.errors.full_messages).to include "Password is too short (minimum is 6 characters)"
       end
         #email 一意性のテスト
 
-      #it '重複したemailが存在する場合登録できない' do
+      it '重複したemailが存在する場合登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
         
-        #@user.save
-        #another_user = FactoryBot.build(:user, email: @user.email)
-        #another_user.email = @user.email
-        #another_user.valid?
-        
-        #expect(another_user.errors.full_messages).to include "Email has already been taken"
-     #end  
+        expect(another_user.errors.full_messages).to include "Email has already been taken"
+      end  
 
       it 'passwordとpassword_confirmationが不一致では登録できない' do
-        @user.password_confirmation = ''
+        @user.password = '123456'
+        @user.password_confirmation = '1234567'
         @user.valid?
+        
         expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
       end
+      it 'last_nameが空では登録できない' do
+        @user.last_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Last name can't be blank"
+      end
+      
+      it 'first_nameが空では登録できない' do
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "First name can't be blank"
+      end
 
-    
-
-    #it 'last_nameが空では登録できない'　do
-      #@user.last_name = ''
-      #@user.valid?
-      #expect(@user.errors.full_messages).to include "Last_name can't be blank"
-    #end
-  
+      
+    end
   end
-end
+
