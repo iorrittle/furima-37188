73fb82de1e1404fed590d700b2a324d7d224880d
iori matumoto <machulive.822@gmail.com>
2021-12-01 18:54:09 +0900

    itemモデルのバリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,19 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :choice
 
+  validates :user,              null: false, foreign_key: true  
+  validates :item_name,         null: false                     
+  validates :price,             null: false
+  validates :explanation,       null: false                    
+  validates :category_id,       null: false                     
+  validates :condition_id,      null: false                     
+  validates :delivery_fee_id,   null: false                     
+  validates :prefecture_id,     null: false                     
+  validates :ship_date_id,      null: false
+
+
+
+
   validates :choice_id, numericality: { other_than: 1 } 
 
 end
