47ef84ca3da501d262ac5b153f9da4ea7b62776f
iori matumoto <machulive.822@gmail.com>
2021-12-02 12:33:08 +0900

    ActiveHashのバリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,7 +20,9 @@ class Item < ApplicationRecord
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank"}
-
+  validates :delivery_fee_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :ship_date_id, numericality: { other_than: 1, message: "can't be blank"}
 end
 
 
