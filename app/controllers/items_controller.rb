a08ae03c665b4583e2cb5e091f9a2a12b72aecf0
iori matumoto <machulive.822@gmail.com>
2021-12-03 19:05:54 +0900

    move_to_indexの削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-  before_action :move_to_index, except: [:index]
+  
   
   def index
     @items = Item.all
@@ -26,11 +26,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :item_name, :price, :explanation, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :ship_date_id).merge(user_id: current_user.id)
   end
   
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
+  
   
   
 
