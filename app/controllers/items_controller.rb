a7521de0da2f5b0e0ac57201c2848f5f6c99dbd5
iori matumoto <machulive.822@gmail.com>
2021-12-05 16:34:50 +0900

    重複した記述をbeforeactionでまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show, :update]
   before_action :move_to_index, only: :edit
   
   def index
@@ -22,15 +23,15 @@ class ItemsController < ApplicationController
     end
   end
   def show
-    @item = Item.find(params[:id])
+    
   end  
 
   def edit
-    @item = Item.find(params[:id])
+    
   end  
 
   def update
-    @item = Item.find(params[:id])
+    
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -45,12 +46,18 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :item_name, :price, :explanation, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :ship_date_id).merge(user_id: current_user.id)
   end
   
+  def set_item
+    @item = Item.find(params[:id])
+  end  
+
   def move_to_index
     @item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == @item.user_id
+    unless current_user.id == @item.user_id
       redirect_to action: :index
     end
   end
+  
+
 
 end
 
