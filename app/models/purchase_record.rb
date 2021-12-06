8a793156b61094a42ff67f59f157ec9a6a47ae7a
iori matumoto <machulive.822@gmail.com>
2021-12-06 15:13:36 +0900

    sendingテーブルの作成とアソシエーションの記述

--- app/models/purchase_record.rb
+++ app/models/purchase_record.rb
@@ -1,4 +1,6 @@
 class PurchaseRecord < ApplicationRecord
-
+  belongs_to  :item
+  belongs_to  :user
+  has_one     :sending
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_06_051502) do
+ActiveRecord::Schema.define(version: 2021_12_06_055215) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -57,6 +57,19 @@ ActiveRecord::Schema.define(version: 2021_12_06_051502) do
     t.index ["user_id"], name: "index_purchase_records_on_user_id"
   end
 
+  create_table "sendings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "purchase_record_id", null: false
+    t.string "postal_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "address", null: false
+    t.string "building"
+    t.string "telephone_number", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["purchase_record_id"], name: "index_sendings_on_purchase_record_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -79,4 +92,5 @@ ActiveRecord::Schema.define(version: 2021_12_06_051502) do
   add_foreign_key "items", "users"
   add_foreign_key "purchase_records", "items"
   add_foreign_key "purchase_records", "users"
+  add_foreign_key "sendings", "purchase_records"
 end
