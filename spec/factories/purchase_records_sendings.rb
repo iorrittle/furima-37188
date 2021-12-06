9806e0b52334bda3e3058bb0125d1b7907803821
iori matumoto <machulive.822@gmail.com>
2021-12-06 21:50:35 +0900

    factorybotの作成

---
+++ spec/factories/purchase_records_sendings.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :purchase_records_sending do
+    
+  end
+end
---
+++ spec/models/purchase_records_sending_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe PurchaseRecordsSending, type: :model do
+  describe ''
+end
